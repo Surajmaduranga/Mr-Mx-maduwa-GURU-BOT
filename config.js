c4ea8eaababc8c4540629fb051b48b3d672180ae
Surajmaduranga <138297207+Surajmaduranga@users.noreply.github.com>
2023-10-31 17:56:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94763933826', 'Maduwa', true],
+  ['94763933826', 'Ash', true], 
   [''] 
 ] //Number of owners
 
